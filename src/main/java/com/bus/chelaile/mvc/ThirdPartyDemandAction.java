85a7a925a1775efa0884db260cb934539785ec33
qiekkdegit <daiding106@126.com>
2018-04-03 10:34:25 +0800

    提交乐元素回调接口

---
+++ src/main/java/com/bus/chelaile/mvc/ThirdPartyDemandAction.java
@@ -0,0 +1,38 @@
+package com.bus.chelaile.mvc;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aliyun.openservices.shade.com.alibaba.fastjson.JSONObject;
+import com.bus.chelaile.common.AnalysisLog;
+import com.bus.chelaile.common.Constants;
+import com.bus.chelaile.service.ServiceManager;
+
+/*
+ * 第三方临时的一些需求
+ */
+@Controller
+@RequestMapping("")
+public class ThirdPartyDemandAction extends AbstractController {
+    @Resource
+    private ServiceManager serviceManager;
+    Logger logger = LoggerFactory.getLogger(ThirdPartyDemandAction.class);
+
+    @RequestMapping(value = "adv!leyuansuActivationBack.action", produces = "Content-Type=text/plain;charset=UTF-8")
+    public String activationBack(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+
+        AdvParam param = getActionParam(request);
+        String appid = request.getParameter("appid");
+        logger.info("来自乐元素的回调请求： imei={}, idfa={}, appid={}", param.getImei(), param.getIdfa(), appid);
+        AnalysisLog.info("leyuansu callback, imei={}, idfa={}, appid={}", param.getImei(), param.getIdfa(), appid);
+
+        return serviceManager.getClienSucMapWithNoHead(new JSONObject(), Constants.STATUS_REQUEST_SUCCESS);
+    }
+}
