ee6e2159234ae9d28a3ef7e454dbea2d1865b2b0
qiekkdegit <daiding106@126.com>
2018-04-17 18:20:07 +0800

    刚刚点击数控制

--- src/main/java/com/bus/chelaile/kafka/thread/ConsumerHandle.java
+++ src/main/java/com/bus/chelaile/kafka/thread/ConsumerHandle.java
@@ -50,6 +50,7 @@ public class ConsumerHandle implements Runnable {
 	// return 6: 广告点击日志
 	// return 0: 其他
 	private int filterContent(String str) {
+//	    logger.info("str={}", str);
 		if ((str.contains(Constants.AD_DOMAIN_NAME) || str.contains(Constants.REDIRECT_DOMAIN_NAME))
 				&& (str.contains(Constants.PARAM_AD_ID) && !str.contains(Constants.FOR_DEVELOP_EXHIBIT))) { // 广告点击
 //			logger.info("读取到广告点击日志= {}", str);
