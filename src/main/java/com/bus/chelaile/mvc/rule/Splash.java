47e15c85c2b21fd4338e0eeff9aa3fd280fd76d6
qiekkdegit <daiding106@126.com>
2018-06-05 21:53:26 +0800

    123

--- src/main/java/com/bus/chelaile/mvc/rule/Splash.java
+++ src/main/java/com/bus/chelaile/mvc/rule/Splash.java
@@ -1,17 +1,17 @@
-package com.bus.chelaile.mvc.rule;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@RequestMapping("/js/android/js/rule")
-public class Splash {
-    
-    @ResponseBody
-    @RequestMapping("/splash.js")
-    public Object splash(){
-        return "hello";
-    }
-
-}
+//package com.bus.chelaile.mvc.rule;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//@RequestMapping("/js/android/js/rule")
+//public class Splash {
+//    
+//    @ResponseBody
+//    @RequestMapping("/splash.js")
+//    public Object splash(){
+//        return "hello";
+//    }
+//
+//}
