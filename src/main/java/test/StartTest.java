480bd200b444e3111052992da23a262913a6dd68
zhangzhizhen <zhizheng.zhang@chelailet.net.cn>
2018-08-07 10:32:53 +0800

    zzz

--- src/main/java/test/StartTest.java
+++ src/main/java/test/StartTest.java
@@ -1,12 +1,21 @@
 package test;
 
+import java.io.IOException;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.bus.chelaile.common.cache.ICache;
 import com.bus.chelaile.service.ServiceManager;
 import com.bus.chelaile.service.StartService;
+import com.bus.chelaile.service.impl.OtherManager;
 
-public class StartTest {
+import junit.framework.TestCase;
+
+public class StartTest extends TestCase{
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
@@ -22,5 +31,28 @@ public class StartTest {
 		System.out.println("success");
 
 	}
+	@Test
+    public void testToEuros() throws IOException {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"classpath:servicebiz/locator-baseservice.xml");
+		
+		StartService ss = context.getBean(StartService.class);
+		
+		ss.init();
+		
+		OtherManager other = context.getBean(OtherManager.class);
+		
+		//other.doServiceList(advParam, showType, queryParam)
+ 
+    	ICache mock = EasyMock.createMock(ICache.class);
+        EasyMock.expect(mock.get("nihao")).andReturn("1.2");
+        EasyMock.expect(mock.get("nihao1")).andReturn("3.2").andReturn("8");
+        EasyMock.replay(mock);
+        System.out.println(mock.get("nihao"));
+        System.out.println(mock.get("nihao1"));
+        System.out.println(mock.get("nihao1"));
+        System.out.println(mock.get("nihao1")+"12");
+    }
 
 }
